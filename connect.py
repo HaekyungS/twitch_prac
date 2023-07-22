943a2e0f8a60646c2c1852116c938c9c9bb622e3
HaekyungSeong <haekyungs95@gmail.com>
2023-07-23 00:42:49 +0900

    아이디와 토큰 노출 방지

--- connect.py
+++ connect.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
 
-Token = 'MTEzMjMyNTE5NTg1Njc0ODYxNA.Gh5ugU.5xbaLThIKP8JjlG0Bkqy2D9s1yrZ-yTF2Wf6uo'
-Chnnel_ID = '1128273006830043156'
+Token = '토큰'
+Chnnel_ID = '채널아이디'
 
 
 class MyClient(discord.Client):
