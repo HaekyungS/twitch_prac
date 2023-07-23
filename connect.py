a7973111a9bc321ad5e464f58bebaf79cbc24e24
HaekyungSeong <haekyungs95@gmail.com>
2023-07-23 09:27:13 +0900

    Revert "파이썬을 이용한 챗봇 및 트위치 연동 제작, 질의 응답"
    This reverts commit 2f802cc8f06193d728ea7e8b7d4d8710a8dbc5d9.

--- connect.py
+++ connect.py
@@ -1,88 +1,20 @@
-# -*- coding: utf-8 -*-
-
 import discord
-import requests
-from json import loads
-import asyncio
+from discord.ext import commands
 
-
-Token = 'MTEzMjMyNTE5NTg1Njc0ODYxNA.GL0N0w.ltOgxOXnO-rRqQFB6uUeKJ3Eb6u_Z0LTzwULMI'
-Channel_ID_natural = 1128273006830043156
-Channel_ID_bangOn = 1132309299222290522
-
-twicth_Client_ID = 'ha60ulmi9w4cnyv0agnq1p27healtd'
-twitch_Secert_Key = '0lfnxdekizo5mfyknuiolm8pi9ocka'
-Aka_ID = 'rh_ryu'
-ment = '나 보러 안올거야?'
-offline = '내가 보고 싶다면 류튜브 봐줘!!'
-
-# channel_bangOn = discord.Client().get_channel(Channel_ID_bangOn)
-# channel_natural = discord.Client().get_channel(Channel_ID_natural)
+Token = 'MTEzMjMyNTE5NTg1Njc0ODYxNA.Gh5ugU.5xbaLThIKP8JjlG0Bkqy2D9s1yrZ-yTF2Wf6uo'
+Chnnel_ID = '1128273006830043156'
 
 
 class MyClient(discord.Client):
-
     async def on_ready(self):
-        channel_natural = self.get_channel(Channel_ID_natural)
-        channel_bangOn = self.get_channel(Channel_ID_bangOn)
-
-        await channel_natural.send('아카가 지구다, 세상이다, 최고다')
-        await channel_bangOn.send('똑똑히 봐라! 이게 아카다! 이게 아카이로 류다!')
-
-        oauth_key = requests.post("https://id.twitch.tv/oauth2/token?client_id=" + twicth_Client_ID +
-                                  "&client_secret=" + twitch_Secert_Key + "&grant_type=client_credentials")
-
-        access_token = loads(oauth_key.text)["access_token"]
-        token_type = 'Bearer '
-        authorization = token_type + access_token
-
-        print(authorization)
-
-        check = False
-
-        while True:
-            print("ready on Notification")
-
-          # 트위치 api에게 방송 정보 요청
-
-            headers = {'client-id': twicth_Client_ID,
-                       'Authorization': authorization}
-
-            response_channel = requests.get(
-                'https://api.twitch.tv/helix/streams?user_login=' + Aka_ID, headers=headers)
-
-            print(response_channel.text)
-
-        # 라이브 상태 체크
-
-            try:
-                # 방송 정보에서 'data'에서 'type' 값이 live 이고 체크상태가 false 이면 방송 알림(오프라인이면 방송정보가 공백으로 옴)
-                if loads(response_channel.text)['data'][0]['type'] == 'live' and check == False:
-                    await channel_bangOn.send(ment + '\n https://www.twitch.tv/' + Aka_ID)
-                    print("Online")
-                    check = True
-
-            except:
-                # await channel_bangOn.send(offline+'\n https://www.youtube.com/@ryuch.821')
-                print("Offline")
-                check = False
-
-            await asyncio.sleep(30)
+        print('Logged on as', self.user)
 
     async def on_message(self, message):
         if message.author == self.user:
             return
 
-        if message.content in ('아카이로 류', '류', '아카', '대장'):
-            await message.channel.send('옴총 귀여워. 알아?')
-
-        if '생일' in message.content and '언제' in message.content:
-            await message.channel.send('그것도 몰라? 11월 1일이잖아!! 난 이런 게 서운해🥺')
-
-        if message.content in ('류튜브', '유튜브', '보고싶다'):
-            await message.channel.send(offline+'\n https://www.youtube.com/@ryuch.821')
-
-    # async def BangOn_message(self, messege):
+        if message.content == 'ping':
+            await message.channel.send('pong')
 
 
 intents = discord.Intents.default()
